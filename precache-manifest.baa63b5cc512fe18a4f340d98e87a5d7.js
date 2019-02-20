779b9a532b39eb0f5f14abf89a67409f4ebf2bd9
BharatKonka <bharat.bhushan.konka@gmail.com>
2019-02-20 10:40:01 +0100

    Updates

---
+++ precache-manifest.baa63b5cc512fe18a4f340d98e87a5d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbb7093a91e2a017fe24cdd0e6dc9868",
+    "url": "/opz_midi/static/media/opz_dails.fbb7093a.png"
+  },
+  {
+    "revision": "b5331b9a3f4c42810d46",
+    "url": "/opz_midi/static/js/runtime~main.b5331b9a.js"
+  },
+  {
+    "revision": "0392c54686f38428d3b0",
+    "url": "/opz_midi/static/js/main.0392c546.chunk.js"
+  },
+  {
+    "revision": "b299376a36a874d95388",
+    "url": "/opz_midi/static/js/2.b299376a.chunk.js"
+  },
+  {
+    "revision": "0392c54686f38428d3b0",
+    "url": "/opz_midi/static/css/main.42e52f13.chunk.css"
+  },
+  {
+    "revision": "b299376a36a874d95388",
+    "url": "/opz_midi/static/css/2.d8297404.chunk.css"
+  },
+  {
+    "revision": "596ed239cb35838bc51ad9ff1cd98d95",
+    "url": "/opz_midi/index.html"
+  }
+];
