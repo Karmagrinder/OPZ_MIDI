b9523eb133c825363a3bca60067c177b38aecaae
Karmagrinder <bharat.bhushan.konka@gmail.com>
2019-02-26 22:20:19 +0100

    InstrumentTrack uses custom Icons

--- src/components/InstrumentTrack.js
+++ src/components/InstrumentTrack.js
@@ -5,8 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './InstrumentsTrackHandler.css';
 import Card from 'react-bootstrap/Card';
 import StyledProgressbar from './StyledProgressBar';
-import { MdSettingsInputComponent} from 'react-icons/md';
-import { IoIosAnalytics, IoIosStats, IoMdOptions} from 'react-icons/io';
+import GetIcon from './IconsLib';
 
 
 //colors
@@ -17,6 +16,8 @@ const red = "#C62937";
 //const grey = "#787878";
 //const opzGrey = "#646464";
 const purple = "#9e66c1";
+const pageIconSize = 35;
+const white = "#ffffff"
 
 function InstrumentTrack(props){
     return (
@@ -24,7 +25,7 @@ function InstrumentTrack(props){
             <Card bg='dark' style={{ color: blue}} >
                 <Card bg="dark" text='white'>
                     <Row>
-                        <Col><Card.Title><IoMdOptions size={25}/></Card.Title></Col>
+                        <Col><Card.Title><GetIcon iconName="Params" size={pageIconSize} color={white}/></Card.Title></Col>
                         <Col><StyledProgressbar percentage={props.track.p1} text={`P1:${props.track.p1}`} color={green} /></Col>
                         <Col><StyledProgressbar percentage={props.track.p2} text={`P2:${props.track.p2}`} color={blue} /></Col>
                         <Col><StyledProgressbar percentage={props.track.filter} text={`Filter:${props.track.filter}`} color={yellow} /></Col>
@@ -33,25 +34,25 @@ function InstrumentTrack(props){
                 </Card>
                 <Card bg="dark" text='success'  >
                     <Row>
-                        <Col><Card.Title><IoIosStats size={25}/></Card.Title></Col>
+                        <Col><Card.Title><GetIcon iconName="Envelope1" size={50} color={green} strokeWidth={5}/></Card.Title></Col>
                         <Col><StyledProgressbar percentage={props.track.attack} text={`A:${props.track.attack}`} color={green} /></Col>
                         <Col><StyledProgressbar percentage={props.track.decay} text={`D:${props.track.decay}`} color={blue} /></Col>
                         <Col><StyledProgressbar percentage={props.track.sustain} text={`S:${props.track.sustain}`} color={yellow} /></Col>
                         <Col><StyledProgressbar percentage={props.track.release} text={`R:${props.track.release}`} color={red} /></Col>
                     </Row>
                 </Card>
-                <Card bg="dark" style={{ color: purple }}  >
+                <Card bg="dark" style={{ color: purple }}>
                     <Row>
-                        <Col><Card.Title><IoIosAnalytics size={25}/></Card.Title></Col>
+                        <Col><Card.Title><GetIcon iconName="SineWave" size={pageIconSize} color={purple} /></Card.Title></Col>
                         <Col><StyledProgressbar percentage={props.track.depth} text={`Depth:${props.track.depth}`} color={green} /></Col>
                         <Col><StyledProgressbar percentage={props.track.rate} text={`Rate:${props.track.rate}`} color={blue} /></Col>
                         <Col><StyledProgressbar percentage={props.track.dest} text={`Target:${props.track.dest}`} color={yellow} /></Col>
                         <Col><StyledProgressbar percentage={props.track.shapeVal} text={`${props.track.shape}`} color={red} /></Col>
                     </Row>
                 </Card>
-                <Card bg="dark" style={{ color: yellow }}  >
+                <Card bg="dark" style={{ color: yellow }}>
                     <Row>
-                        <Col><Card.Title><MdSettingsInputComponent size = {25}/></Card.Title></Col>
+                        <Col><Card.Title><GetIcon iconName="Master1" size={pageIconSize} color={yellow} /></Card.Title></Col>
                         <Col><StyledProgressbar percentage={props.track.fx1} text={`Fx1:${props.track.fx1}`} color={green} /></Col>
                         <Col><StyledProgressbar percentage={props.track.fx2} text={`Fx2:${props.track.fx2}`} color={blue} /></Col>
                         <Col><StyledProgressbar percentage={props.track.pan} text={`Pan:${props.track.pan}`} color={yellow} /></Col>
