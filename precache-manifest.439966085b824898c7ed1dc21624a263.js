596641eed2508064f4d2d40dc2eecb5705867e7d
Karmagrinder <bharat.bhushan.konka@gmail.com>
2019-02-27 23:42:52 +0100

    Updates

--- precache-manifest.439966085b824898c7ed1dc21624a263.js
+++ precache-manifest.439966085b824898c7ed1dc21624a263.js
@@ -1,22 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "fbb7093a91e2a017fe24cdd0e6dc9868",
-    "url": "/OPZ_MIDI/static/media/opz_dails.fbb7093a.png"
-  },
-  {
     "revision": "3cbafe98c29f8adc28af",
     "url": "/OPZ_MIDI/static/js/runtime~main.3cbafe98.js"
   },
   {
-    "revision": "dcabc3d4d475c887ee7c",
-    "url": "/OPZ_MIDI/static/js/main.dcabc3d4.chunk.js"
+    "revision": "7e301168d3668e22ee4f",
+    "url": "/OPZ_MIDI/static/js/main.7e301168.chunk.js"
   },
   {
     "revision": "2739300294ca86af05fc",
     "url": "/OPZ_MIDI/static/js/2.27393002.chunk.js"
   },
   {
-    "revision": "dcabc3d4d475c887ee7c",
+    "revision": "7e301168d3668e22ee4f",
     "url": "/OPZ_MIDI/static/css/main.99a5a68b.chunk.css"
   },
   {
@@ -24,7 +20,7 @@ self.__precacheManifest = [
     "url": "/OPZ_MIDI/static/css/2.7dcee7f3.chunk.css"
   },
   {
-    "revision": "428c8dc636d98d5ab5b1fe16e36f37d9",
+    "revision": "8d745eb48ca325950eafe0495c3a61ad",
     "url": "/OPZ_MIDI/index.html"
   }
 ];
